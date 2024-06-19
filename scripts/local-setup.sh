6e73d099e68863a0acb0437bed091395b190f6b4
yejiayu <yejiayu.fe@gmail.com>
2024-06-19 23:00:11 +0800

    update

--- scripts/local-setup.sh
+++ scripts/local-setup.sh
@@ -71,7 +71,7 @@ fi
 echo "$HOMEDIR"
 	echo "$VAL_MNEMONIC" | station-evm keys add "$VAL_KEY"  --keyring-backend "$KEYRING" --algo "$KEYALGO" --home "$HOMEDIR"
 	# Set moniker and chain-id for Evmos (Moniker can be anything, chain-id must be an integer)
-	station-evm init $MONIKER -o --chain-id "$CHAINID" --home "$HOMEDIR"
+	station-evm init $MONIKER -o --chain-id "$CHAINID" --home "$HOMEDIR" -y
 	# Change parameter token denominations to aevmos
 	jq '.app_state["staking"]["params"]["bond_denom"]="aevmos"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
 	jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="aevmos"' "$GENESIS" >"$TMP_GENESIS" && mv "$TMP_GENESIS" "$GENESIS"
